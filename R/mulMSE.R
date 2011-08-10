3171b68c07be54fb9e29f3ed000b0793a4d5b04d
Claudio Isella <claudio.isella@ircc.it>
2011-08-10 17:26:45 +0000

    " new 1.2.4 "
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Mulcom@57361 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mulMSE.R
+++ R/mulMSE.R
@@ -4,9 +4,7 @@ function( vector, index, tmp = vector()){
       tmo <- vector[ ( length( vector ) / 2 + 1 ):length( vector ) ]-vector[ 1 : ( length( vector ) / 2 )]
       d <- as.factor( index[ length( vector ):( length( vector ) / 2 + 1 ) ] )
       names( tmo ) <- d
-      #return(tmo)
       hm <- harmonicMean( names ( tmo ) )
-      #return(hm)
       b <- sqrt( sum( mulSSE( tmo, d ) )/( length( tmo ) - length( levels( factor( d ) ) ) ) * 2 / hm )
       return( b )
    }else{

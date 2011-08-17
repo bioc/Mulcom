b8e1b15f990bc6379e4c415e162cae0a4866e937
Claudio Isella <claudio.isella@ircc.it>
2011-08-17 14:50:45 +0000

    "new 1.2.5 update"
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_8/madman/Rpacks/Mulcom@57496 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mulPerm.R
+++ R/mulPerm.R
@@ -1,46 +1,63 @@
-mulPerm <- function (eset, index, np = 10, seed = 1){
-    if (class(eset) == "ExpressionSet" & is.vector(index) & is.numeric(np) &
-        is.numeric(seed)) {
-        mul <- new("MULCOM_P")
-        b <- mulScores(eset, index)
-        mul@FC <- b@FC
-        mul@MSE_Corrected <- b@MSE_Corrected
-        set.seed(seed)
-        fc <- vector()
-        mse <- vector()
-        quant <- quantile(1:np, seq(0, 1, 0.1))
-        h <- 1
-        ind <- quant[h]
-        message("MulCom permutation starts")
-        packageStartupMessage("initializing ...", appendLF = TRUE)
-        for (i in 1:np) {
-            tmp <- sample(index)
-            while (min(tapply(sample(index), as.vector(index),
-                function(x) {
-                  length(unique(x))
-                })) == 1) {
-                tmp <- sample(index)
-            }
+mulPerm <- function(eset, index, np, seed, segm = "F"){
+   if (class(eset) == "ExpressionSet" & is.vector(index) & is.numeric(np) & is.numeric(seed) | class(eset) == "matrix" | class(eset) == "data.frame") {
+#      if(class(eset) == "ExpressionSet"){
+          mul <- new("MULCOM_P")
+          multest <- mulScores(eset, index)
+          mul <- new("MULCOM_P")
+          mul@FC <- multest@FC
+          mul@MSE_Corrected <- multest@MSE_Corrected
+          set.seed(seed)
+          fc <- vector()
+          mse <- vector()
+          ngroups <- length(levels(factor(index)))
+          means <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) * np ) ) )
+          mse <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) * np ) ) )
+          n <- as.integer((dim(eset))[1])
+          m <- as.integer((dim(eset))[2])
+          reference <- c(0)
+          rand_ind <- mulIndex(index, np = np, seed = seed)
+          if(segm == "F"){
+             mul_out <- mulPermC(eset, rand_ind, means, mse, n, m, nump=np, ngroups, reference)
+             mul@FCp <- as.matrix(mul_out[[3]])
+             dim(mul@FCp) <- c(c(ngroups -1),dim(eset)[1],np)
+             mul@MSE_Correctedp <- as.matrix(mul_out[[4]])
+             dim(mul@MSE_Correctedp) <- c(c(ngroups -1),dim(eset)[1],np)
+             return(mul)
+          }else{
+             npst <- np
+             np <- vector()
+             stps <- seq(1, npst, by=segm)
+             lst <- list();
+             for(i in 1:c(length(stps)-1)){
+                lst[[i]] <- stps[i]:c(stps[c(i+1)]-1)
+             }
+             lst[[i+1]]<-stps[c(i+1)]:npst
+             fcs <- vector()
+             mses <- vector()
+             for(i in 1:length(lst)){
+                rand_tmp <- rand_ind[,lst[[i]]]
+                nump <- length(lst[[i]])
+                means <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) * nump ) ) )
+                mse <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) * nump ) ) )
+                mul_out <- mulPermC(eset, rand_tmp, means, mse, n, m, nump, ngroups, reference)
+                fcs <- c(fcs,as.matrix(mul_out[[3]]))
+                mses <- c(mses,as.matrix(mul_out[[4]]))
+             }
+             np <- npst
+             mul@FCp <- as.array(fcs)
+             dim(mul@FCp) <- c(c(ngroups -1),dim(eset)[1],np)
+             mul@MSE_Correctedp <- as.array(mses)
+             dim(mul@MSE_Correctedp) <- c(c(ngroups -1),dim(eset)[1],np)
+             return(mul)
+          }
+#      }else{
+#         print("hello")
+#      }
+   }else{
 
-            if(i > ind){
-               h <- h+1
-               ind <- quant[h]
-               packageStartupMessage(paste(names(quant[h]), "\r"), appendLF = FALSE)
-            }
-            a <- mulScores(eset, tmp)
-            fc <- cbind(fc, a@FC)
-            mse <- cbind(mse, a@MSE_Corrected)
-        }
-        packageStartupMessage("Done")
-        dim(fc) <- c(dim(a@FC), np)
-        mul@FCp <- fc
-        dim(mse) <- c(dim(a@MSE_Corrected), np)
-        mul@MSE_Correctedp <- mse
-        return(mul)
-    }
-    else {
-        stop("error in input files", call. = FALSE)
-    }
+      stop("error in input files", call. = FALSE)
+
+   }
 }
 
 
--- R/mulScores.R
+++ R/mulScores.R
@@ -1,29 +1,80 @@
-mulScores <-
-function(eset, index){
-   if(class(eset) == "ExpressionSet" & is.vector(index) ){
-      mulcom <- new("MULCOM")
-      data <- as.vector(as.matrix(exprs(eset)))
-      groups <- index
-      ngroups <- length(levels(factor(groups)))
-      means <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) ) ) )
-      SS <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ngroups ) ) )
-      harmonic_means <- c(seq(0, 0, length=ngroups-1 ) )
-      n <- as.integer((dim(eset))[1])
-      m <- as.integer((dim(eset))[2])
-      reference <- c( 0 )
-      means <- .C("fast_mean", as.double(data), as.double(means), as.integer(n), as.integer(m), as.integer(groups), as.integer(reference),PACKAGE = "Mulcom")
-      harm <- .C("fast_harmonic_sample_size", as.double(harmonic_means), as.integer(m), as.integer(groups), as.integer(reference),PACKAGE = "Mulcom")
-      harmDF <- data.frame(matrix(harm[[1]], ncol=ngroups-1,byrow=TRUE))
-      mulcom@FC <- t(data.frame(matrix(means[[2]], ncol=(ngroups-1), byrow=TRUE), row.names=featureNames(eset)))
-      sumsq <- .C("fast_SS", as.double(data), as.double(SS) , as.integer(n), as.integer(m), as.integer(groups),PACKAGE = "Mulcom")
-      sumsqDF <- data.frame(matrix(sumsq[[2]], ncol=ngroups,byrow=TRUE), row.names=featureNames(eset))
-      df <- length(groups) - ngroups
-      sss <-  apply(sumsqDF,1,sum)
-      mse <- sqrt((sss*2/df)%*%harmDF^-1)
-      mulcom@MSE_Corrected <- t(mse)
-      return(mulcom)
-   }else{ 
-      stop("error in input files", call. = FALSE)
+mulScores <- function(eset, index){
+
+   if(class(eset) == "ExpressionSet" & is.vector(index) | class(eset) == "matrix" | class(eset) == "data.frame"){
+
+      if(class(eset) == "ExpressionSet"){
+
+         mulcom <- new("MULCOM")
+         data <- as.vector(as.matrix(exprs(eset)))
+
+         groups <- index
+         ngroups <- length(levels(factor(groups)))
+
+         n <- as.integer((dim(eset))[1])
+         m <- as.integer((dim(eset))[2])
+         reference <- c( 0 )
+
+         means <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) ) ) )
+         SS <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ngroups ) ) )
+         harmonic_means <- c(seq(0, 0, length=ngroups-1 ) )
+         sss2 <- c( seq( 0, 0, length = as.integer((dim(eset))[1]) ) )
+         mse <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) ) ) )
+
+         out <- .C("Single_SimulationC", as.double(data), as.double(means), as.double(harmonic_means), as.double(SS),  as.double(sss2), as.double(mse), as.integer(n), as.integer(m), as.integer(groups), as.integer( ngroups ), as.integer(reference),PACKAGE = "Mulcom")
+
+         mulcom@FC <- t(data.frame(matrix(out[[2]], ncol=(ngroups-1), byrow=TRUE), row.names=featureNames(eset)))
+         mulcom@HM <- matrix(out[[3]], ncol=ngroups-1,byrow=TRUE)
+#         sumsqDF <- data.frame(matrix(out[[4]], ncol=ngroups,byrow=TRUE), row.names=featureNames(eset))
+#         sss2 <- data.frame(matrix(out[[5]], ncol=1,byrow=TRUE), row.names=featureNames(eset))
+         mse <- data.frame(matrix(out[[6]], ncol=ngroups-1,byrow=TRUE), row.names=featureNames(eset))
+
+         mulcom@MSE_Corrected <- t(mse)
+
+         return(mulcom)
+
+      }else{
+
+      if(class(eset) == "data.frame" & is.vector(index)){ eset <- as.matrix(eset)}
+
+         if(class(eset) == "matrix" & is.vector(index) ){
+
+            mulcom <- new("MULCOM")
+            data <- as.vector(eset)
+
+            groups <- index
+            ngroups <- length(levels(factor(groups)))
+
+            n <- as.integer((dim(eset))[1])
+            m <- as.integer((dim(eset))[2])
+            reference <- c( 0 )
+
+            means <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) ) ) )
+            SS <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ngroups ) ) )
+            harmonic_means <- c(seq(0, 0, length=ngroups-1 ) )
+            sss2 <- c( seq( 0, 0, length = as.integer((dim(eset))[1]) ) )
+            mse <- c( seq ( 0, 0, length = ( (dim(eset))[1] * ( ngroups - 1 ) ) ) )
+
+            out <- .C("Single_SimulationC", as.double(data), as.double(means), as.double(harmonic_means), as.double(SS),  as.double(sss2), as.double(mse), as.integer(n), as.integer(m), as.integer(groups), as.integer( ngroups ), as.integer(reference),PACKAGE = "Mulcom")
+
+            mulcom@FC <- t(data.frame(matrix(out[[2]], ncol=(ngroups-1), byrow=TRUE), row.names=rownames(eset)))
+            mulcom@HM <- matrix(out[[3]], ncol=ngroups-1,byrow=TRUE)
+#            sumsqDF <- data.frame(matrix(out[[4]], ncol=ngroups,byrow=TRUE), row.names=rownames(eset))
+#            sss2 <- data.frame(matrix(out[[5]], ncol=1,byrow=TRUE), row.names=rownames(eset))
+            mse <- data.frame(matrix(out[[6]], ncol=ngroups-1,byrow=TRUE), row.names=rownames(eset))
+
+            mulcom@MSE_Corrected <- t(mse)
+
+            return(mulcom)
+
+         }
+
+      }
+
+   }else{
+
+         stop("error in input files", call. = FALSE)
+
    }
+
 }
 
